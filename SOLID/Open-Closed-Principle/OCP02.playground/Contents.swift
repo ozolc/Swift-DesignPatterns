6bb661d900ef5014c8ef38d095d1b13688bd6495
Maksim Nosov <ozolc@yandex.ru>
2019-03-07 17:21:42 +0300

    OCP exampe 02
    If you want to add the possibility to print also the details of a new class, we should change the implementation of printData every time we want to log a new class—breaking OCP

---
+++ SOLID/Open-Closed-Principle/OCP02.playground/Contents.swift
@@ -0,0 +1,58 @@
+// Example of Open-Closed Principle
+
+//If you want to create a class easy to maintain, it must have two important characteristics:
+//
+//Open for extension: You should be able to extend or change the behaviours of a class without efforts.
+//Closed for modification: You must extend a class without changing the implementation.
+
+import Foundation
+
+class Car {
+    let name: String
+    let color: String
+    
+    init(name: String, color: String) {
+        self.name = name
+        self.color = color
+    }
+    
+    func printDetails() -> String {
+        return "I'm \(name) and my color is \(color)"
+    }
+}
+
+class Bicycle {
+    let type: String
+    
+    init(type: String) {
+        self.type = type
+    }
+    
+    func printDetails() -> String {
+        return "I'm a \(type)"
+    }
+}
+
+class Logger {
+    
+    func printData() {
+        let cars = [
+            Car(name: "Batmobile", color: "Black"),
+            Car(name: "SuperCar", color: "Gold"),
+            Car(name: "FamilyCar", color: "Grey")
+        ]
+        
+        cars.forEach { car in
+            print(car.printDetails())
+        }
+        
+        let bicycles = [
+            Bicycle(type: "BMX"),
+            Bicycle(type: "Tandem")
+        ]
+        
+        bicycles.forEach { bicycles in
+            print(bicycles.printDetails())
+        }
+    }
+}
